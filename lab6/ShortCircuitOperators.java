b1e194ce2ee58cd31b6d27ab69ce657ea92e140a
Bartłomiej Dmitruk <bartlomiej.dmitruk.stud@pw.edu.pl>
2022-11-22 18:19:02 +0100

    zadania wykonane na laboratorium nr 6

---
+++ lab6/ShortCircuitOperators.java
@@ -0,0 +1,52 @@
+public class ShortCircuitOperators {
+	private static boolean f1true() {
+		System.out.format("f1true()%n");
+		//System.out.printf("f1true()%n");// try this
+		//System.out.println("f1true()");// try this
+		return true;
+	}
+	
+	private static boolean f1false() {
+		System.out.printf("f1false()%n");
+		return false;
+	}
+
+	private static void leftShift () {
+	for(int i =0; i < Integer.SIZE; i++)
+		System.out.println(1<<i);
+	}
+
+	private static void rightShift (int x) {
+		for(int i =0; i < Integer.SIZE; i++)
+			System.out.println(x>>i);
+	}
+
+	private static void unsignedRightShift (int x) {
+		for(int i =0; i < Integer.SIZE; i++)
+			System.out.println(x>>>i);
+	}
+
+	
+	public static void main(String[] args) {
+		boolean b1, b2, b3, b4, b5, b6, b7, b8;
+
+		leftShift();
+		System.out.println("");
+
+		rightShift(0x80000000);
+		System.out.println("");
+
+		unsignedRightShift(0x80000000);
+		b1 = f1true() & f1true();
+		b2 = f1true() && f1true();
+		
+		b3 = f1false() & f1false();
+		b4 = f1false() && f1false();
+		
+		b5 = f1true() | f1true();
+		b6 = f1true() || f1true();
+		
+		b7 = f1false() | f1false();
+		b8 = f1false() || f1false();
+	}
+}
